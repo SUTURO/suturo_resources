90ea8de51f7b93555b1d9936e1e7b8b64cf25783
JuliSlz <juschulz@uni-bremen.de>
2025-09-25 10:06:11 +0200

    [tmx] added starting pycharm to terminal_serve_breakfast.sh

--- suturo_utils/launch_scripts/terminal_serve_breakfast.sh
+++ suturo_utils/launch_scripts/terminal_serve_breakfast.sh
@@ -17,11 +17,15 @@ tmux send-keys -t "$SESSION:0.1" 'hsr' C-m
 tmux send-keys -t "$SESSION:0.2" 'hsr' C-m
 tmux send-keys -t "$SESSION:0.3" 'hsr' C-m
 
-tmux send-keys -t "$SESSION:0.0" 'roslaunch suturo_bringup GermanOpen_bringup.launch' C-m
+tmux send-keys -t "$SESSION:0.0" 'roslaunch suturo_bringup suturo_tab.launch' C-m
 tmux send-keys -t "$SESSION:0.1" 'robokudo_venv && rosrun robokudo main.py _ae=serve_breakfast _ros_pkg=milestone1' C-m
 # Magic Number
 sleep 2
 tmux send-keys -t "$SESSION:0.2" 'roslaunch giskardpy_ros giskardpy_hsr_real_vel_with_kitchen.launch' C-m
+
+# Magic Number
+sleep 1
+tmux send-keys -t "$SESSION:0.3" 'pycharm' C-m
 
 # Attach to the session
 tmux attach-session -t "$SESSION:0"
